75de30378d659b0585cf36578f854f965faa25f9
Amir Mansoor <69566729+amir-mansoor@users.noreply.github.com>
2023-04-26 12:03:01 +0300

    javascript

---
+++ script.js
@@ -0,0 +1,19 @@
+let video = document.querySelectorAll("video")
+video.forEach(video => {
+    let playPromise = video.play()
+    if(playPromise !== undefined) {
+        playPromise.then(() => {
+            let observer = new IntersectionObserver(entries => {
+                entries.forEach(entry => {
+                    video.muted = false
+                    if(entry.intersectionRatio !== 1 && !video.paused){
+                        video.pause()
+                    } else if (entry.intersectionRatio > 0.5 && video.paused) {
+                        video.play()
+                    }
+                })
+            }, {threshold: 0.5})
+            observer.observe(video)
+        })
+    }
+})
